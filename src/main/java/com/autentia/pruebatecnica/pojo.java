9d59aa2bea2fc26f06fe9129714702e797d34f0a
Mercedes Martin Guerrero <mercedes.martin@autentia.com>
2019-03-28 16:04:02 +0100

    pojo y xml creados

---
+++ src/main/java/com/autentia/pruebatecnica/pojo.java
@@ -0,0 +1,58 @@
+package com.autentia.pruebatecnica;
+
+public class pojo {
+    private int id;
+    private char activo;
+    private String profesor;
+    private String titulo;
+    private String nivel;
+    private int horas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public char getActivo() {
+        return activo;
+    }
+
+    public void setActivo(char activo) {
+        this.activo = activo;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+}
